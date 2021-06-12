6a305470a1464bf32080e8ee8cdb97d83adb6f98
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-12 06:21:37 -0300

    02.06 usando DTO

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,41 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDto {
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriaçao;
+	
+	
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriaçao = topico.getDataCriacao();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public LocalDateTime getDataCriaçao() {
+		return dataCriaçao;
+	}
+
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	}
+	
+	
+}
--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -49,6 +49,14 @@ public class Topico {
 	}
 
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getTitulo() {
 		return titulo;

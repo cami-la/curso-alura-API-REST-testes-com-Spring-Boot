71c1e20e56c256933dd77476ea2d126b1d7fd11d
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-14 05:55:14 -0300

    04.02 Cadastrando tópicos

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -3,20 +3,28 @@ package br.com.alura.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicoController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
+	@Autowired
+	private CursoRepository cursoRepository;
 	
-	@RequestMapping("/topicos")
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		List<Topico> topicos = null;
 		
@@ -27,6 +35,12 @@ public class TopicoController {
 		}
 		
 		return TopicoDto.converter(topicos);
+	}
+	
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
 	}
 	
 }
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+	
+	private String titulo;
+	private String mensagem;
+	private String nomeCurso;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+	
+}

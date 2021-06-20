a579353463722a2b3cc1f5ddb1bbb3336768fa71
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-20 10:35:42 -0300

    03.02 Habilitando o Spring Security

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+
+}
